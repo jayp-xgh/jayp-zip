71057600d23a68b6a56cda2f7354b8c113e360f4
Joao Pereira <joaovitorpereiramartins.2004@gmail.com>
2022-10-03 12:11:45 -0300

    test

--- src/components/home/blog/Blog.jsx
+++ src/components/home/blog/Blog.jsx
@@ -4,13 +4,13 @@ export function Blog() {
       <ConteudoBlog>
         <div id="blog">
           <h1>Blog </h1>
-          <img src="/src/assets/svg/emojBlog.svg" alt="" height="60px" />
+          <img src="assets/svg/emojBlog.svg" alt="" height="60px" />
         </div>
         <p>
           Seja muitíssimo bem-vindo a meu blog!! aqui você poderá conhecer um pouquinho sobre quem sou eu, meus trabalhos e também adquirir conhecimento lendo minhas documentações.
         </p>
         <a href="" className="containerJoaizinhoDev">
-          <img src="/src/assets/img/minhaFoto.png" alt="" height="40px" />
+          <img src="assets/img/minhaFoto.png" alt="" height="40px" />
           <span>
             @joaozinhoDev
           </span>
